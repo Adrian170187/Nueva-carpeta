251b2761a4fb2debc9c16032bbd892ecfd0f089e
Adrian Urrutia <marcelo170187@gmail.com>
2025-10-19 16:19:32 -0300

    se agrego el metodo post

--- tp1api.py
+++ tp1api.py
@@ -6,3 +6,18 @@ contador=1
 @app.get("/mensajes/")
 def obtener_mensajes():
     return mensajes
+
+@app.get("/mensajes/{mensajes_id}")
+def obtener_mensaje(mensajes_id: int):
+    for mensaje in mensajes:
+        if mensaje["id"] == mensajes_id:
+            return mensaje
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado")
+
+@app.post("/mensajes/")
+def crear_mensaje(mensaje: str):
+    global contador
+    nuevo_mensaje = {"id": contador, "mensaje": mensaje}
+    mensajes.append(nuevo_mensaje)
+    contador += 1
+    return nuevo_mensaje
